bbf134f867f5bcbd1b747dea0bc9cc4f7d1a96ef
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-04-03 16:32:09 -0300

    Lista.hpp criada

---
+++ Trabalho03/Lancamento.h
@@ -0,0 +1,26 @@
+/**
+ * Universidade Federal de Santa Catarina
+ * Centro Tecnológico
+ * Departamento de Informática e Estatística
+ * Ciências da Computação
+ * INE5408 - Estruturas de Dados
+ *
+ * Trabalho 03 - Implementação de filas de char com Vetores em C++
+ * Alunos: Antonio Vinicius Gomes Teixeira  Matrícula: 13100731
+ *         Matheus Ben-Hur de Melo Leite    Matrícula: 13100765
+ *
+ * Lancamento.h
+ */
+
+#ifndef LANCAMENTO_H
+#define LANCAMENTO_H 
+
+ class Lancamento { 
+public: 
+	Lancamento();
+	~Lancamento();
+private:
+ 	char *nome;
+ 	double valor;
+ };
+
