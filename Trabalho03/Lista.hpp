ecf4b42edb77f63d2c490d6ec142ff223dcb7250
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-04-03 19:21:29 -0300

    implementação da Lista.hpp em andamento

--- Trabalho03/Lista.hpp
+++ Trabalho03/Lista.hpp
@@ -9,5 +9,69 @@
  * Alunos: Antonio Vinicius Gomes Teixeira  Matrícula: 13100731
  *         Matheus Ben-Hur de Melo Leite    Matrícula: 13100765
  *
- * Lista.h
+ * Lista.hpp
  */
+
+#define TAMANHO_MAX = 100
+#define ERRO_LISTA_CHEIA = -1;
+#define ERRO_LISTA_VAZIA = -2; 
+#define ERRO_POSICAO_INEXISTENTE = -3;
+
+template typename<T>
+ class Lista {
+ public:
+ 	Lista();
+ 	~Lista();
+ 	void inserir(T elemento);
+ 	void inserirNaPosicao(T elemento, int posicao);
+ 	void retirarDaPosicao(int posicao);
+ 	void retirarElemento(T elemento);
+ 	T pegarElemento(T elemento);
+ 	T pegarElementoEm(int posicao);
+ 	bool estaCheia();
+ 	bool estaVazia();
+ 	int posicaoDo(T elemento);
+ 	bool contem(T elemento);
+
+private:
+	T dados[TAMANHO_MAX];
+	int tamanho;
+
+ };
+
+template typename<T>
+Lista<T>::Lista() {
+	tamanho = -1;
+}
+
+template typename<T>
+Lista<T>::~Lista() {
+
+}
+
+template typename<T>
+void Lista<T>::inserir(T elemento) {
+	if(!estaCheia()) {
+		dados[tamanho++]=elemento;	
+		return;
+	}
+	throw (ERRO_LISTA_CHEIA);
+}
+
+template typename<T>	
+Lista<T>::inserirNaPosicao(T elemento, int posicao) {
+	if(posicao =< TAMANHO_MAX & posicao >= 0) {
+		dados[posicao] = elemento;
+		return;
+	}
+	throw (ERRO_POSICAO_INEXISTENTE);
+}
+
+template typename<T>
+Lista<T>::retirarDaPosicao(int posicao) {
+	if(posicao =< TAMANHO_MAX & posicao >= 0) {
+		dados[posicao] = NULL;
+		return; 
+	}
+	throw(ERRO_POSICAO_INEXISTENTE);
+}
