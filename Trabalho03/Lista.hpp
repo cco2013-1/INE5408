8bc0e427a27026f28409d313e0a8bff2467dec92
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-04-08 19:55:24 -0300

    Métodos refatorados para ficarem iguais aos do slide, nem todos implemntados

--- Trabalho03/Lista.hpp
+++ Trabalho03/Lista.hpp
@@ -5,7 +5,7 @@
  * Ciências da Computação
  * INE5408 - Estruturas de Dados
  *
- * Trabalho 02 - Implementação de filas de char com Vetores em C++
+ * Trabalho 03 - Implementação de filas de char com Vetores em C++
  * Alunos: Antonio Vinicius Gomes Teixeira  Matrícula: 13100731
  *         Matheus Ben-Hur de Melo Leite    Matrícula: 13100765
  *
@@ -22,56 +22,121 @@ template typename<T>
  public:
  	Lista();
  	~Lista();
- 	void inserir(T elemento);
- 	void inserirNaPosicao(T elemento, int posicao);
- 	void retirarDaPosicao(int posicao);
- 	void retirarElemento(T elemento);
- 	T pegarElemento(T elemento);
- 	T pegarElementoEm(int posicao);
- 	bool estaCheia();
- 	bool estaVazia();
- 	int posicaoDo(T elemento);
+ 	void adiciona(T elemento); //adiciona o elemento na ultima posicao livre
+ 	void adicionaNoInicio(T elemento); //adiciona o elemento no inicio da lista
+ 	void adicionaNaPosicao(T elemento, int posicao); //adiciona o elemento na posicao desejada
+ 	void adicionaEmOrdem(T elemento); //adiciona o elemento na ordem definida pelo programador
+ 	T retira(); //retira o ultimo elemento da lista
+ 	T retiraDoInicio(); 
+ 	T retiraDaPosicao(int posicao);
+ 	T retiraEspecifico(T elemento); 
+ 	bool listaCheia();
+ 	bool listaVazia();
+ 	int posicao(T elemento);
  	bool contem(T elemento);
-
+ 	bool igual(T elemento1, T elemento2);
+ 	bool maior(T elemento1, T elemento2); //retorna true caso o elemento 1 for maior que o elemento 2 (verificar necessidade)
+ 	bool menor(T elemento1, T elemento2); //retorna true caso o elemento 1 for menor que o elemento 2 (verificar necessidade)
 private:
 	T dados[TAMANHO_MAX];
-	int tamanho;
-
+	int ultimo; //valor da posição do ultimo elemento inserido na lista
+	void deslocarEsquerda(int posicao); //desloca os elementos da lista para a esquerda a partir da posição passada por parâmetro
+	void deslocarDireita(int posicao); //desloca os elementos da lista para a direita a partir da posição passada por parâmetro
  };
 
 template typename<T>
 Lista<T>::Lista() {
-	tamanho = -1;
+	ultimo = -1;
 }
 
 template typename<T>
 Lista<T>::~Lista() {
-
+	ultimo = -1;
 }
 
 template typename<T>
-void Lista<T>::inserir(T elemento) {
+bool Lista<T>::estaCheia() {
+	if(ultimo == TAMANHO_MAX - 1) {
+		return true;
+	}
+	return false;
+}
+
+template typename<T>
+bool Lista<T>::estaVazia() {
+	if(ultimo == -1) {
+		return true;
+	}
+	return false;
+}
+
+template typename<T>
+void Lista<T>::adiciona(T elemento) {
 	if(!estaCheia()) {
-		dados[tamanho++]=elemento;	
+		dados[++ultimo]=elemento;	
 		return;
 	}
 	throw (ERRO_LISTA_CHEIA);
 }
 
-template typename<T>	
-Lista<T>::inserirNaPosicao(T elemento, int posicao) {
-	if(posicao =< TAMANHO_MAX & posicao >= 0) {
-		dados[posicao] = elemento;
-		return;
+template typename<T>
+T Lista<T>::retira() {
+	if(!estaCheia()) {
+		return dados[ultimo--];
 	}
-	throw (ERRO_POSICAO_INEXISTENTE);
+	throw (ERRO_LISTA_CHEIA);
+}
+
+
+/* Os métodos abaixo servirão de base para implementar métodos instanciados acima*/
+
+
+
+template typename<T>	
+void Lista<T>::inserirNaPosicao(T elemento, int posicao) {
+	if(!estaCheia()) {
+		if(posicao =< TAMANHO_MAX & posicao >= 0) {
+			deslocarDireita(posicao);
+			dados[posicao] = elemento;
+			return;
+		}
+		throw (ERRO_POSICAO_INEXISTENTE);
+	}
+	throw (ERRO_LISTA_CHEIA);
 }
 
 template typename<T>
-Lista<T>::retirarDaPosicao(int posicao) {
+void Lista<T>::retirarDaPosicao(int posicao) {
+	if(!estaVazia()) {
+		if(posicao =< TAMANHO_MAX & posicao >= 0) {
+			deslocarEsquerda(posicao);
+			return; 
+		}
+		throw(ERRO_POSICAO_INEXISTENTE);	
+	}
+	throw(ERRO_LISTA_VAZIA);
+	
+}
+
+template typename<T>
+void Lista<T>::retirarElemento(T elemento) {
+
+}
+
+template typename<T>
+T Lista<T>::pegarElemento(T elemento) {
+
+}
+
+template typename<T>
+T Lista<T>::pegarElementoEm(int posicao) {
 	if(posicao =< TAMANHO_MAX & posicao >= 0) {
-		dados[posicao] = NULL;
-		return; 
+		return dados[posicao];
 	}
 	throw(ERRO_POSICAO_INEXISTENTE);
+}
+
+template typename<T>
+bool Lista<T>::estaCheia() {
+	if()
 }
