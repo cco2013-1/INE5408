baf314e90ce3c878a970d2e29d18683b0fd51fff
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-04-10 22:17:02 -0300

    Trabalho04 criado, está no moodle e rodou com 8,5 de nota, falhou somente no teste retiraEspecifico()

---
+++ Trabalho04/ListaDeVetores.cpp
@@ -0,0 +1,25 @@
+/**
+ * Universidade Federal de Santa Catarina
+ * Centro Tecnológico
+ * Departamento de Informática e Estatística
+ * Ciências da Computação
+ * INE5408 - Estruturas de Dados
+ *
+ * Trabalho 04 - Implementação da Lista com um vetor de Ponteiros para Strings
+ * Alunos: Antonio Vinicius Gomes Teixeira  Matrícula: 13100731
+ *         Matheus Ben-Hur de Melo Leite    Matrícula: 13100765
+ *
+ * ListaDeVetores.cpp
+ */
+
+ #include "ListaDeVetores.h"
+
+
+ListaDeVetores::ListaDeVetores() : Lista<char*>(){
+}
+
+ListaDeVetores::~ListaDeVetores() {
+}
+
+
+
---
+++ Trabalho04/ListaDeVetores.h
@@ -0,0 +1,23 @@
+/**
+ * Universidade Federal de Santa Catarina
+ * Centro Tecnológico
+ * Departamento de Informática e Estatística
+ * Ciências da Computação
+ * INE5408 - Estruturas de Dados
+ *
+ * Trabalho 04 - Implementação da Lista com um vetor de Ponteiros para Strings
+ * Alunos: Antonio Vinicius Gomes Teixeira  Matrícula: 13100731
+ *         Matheus Ben-Hur de Melo Leite    Matrícula: 13100765
+ *
+ * ListaDeVetores.h
+ */
+
+ #include "Lista.hpp"
+ 
+
+ class ListaDeVetores: public Lista<char*> {
+ public:
+ 	ListaDeVetores();
+ 	virtual ~ListaDeVetores();
+ };
+
