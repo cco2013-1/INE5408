c2404d600cfa03fb440e49eaace0a10a8f43e36f
Vinicius Teixeira <vinicius0026@gmail.com>
2014-04-11 10:28:40 -0300

    pequenas mudancas

--- Trabalho03/Lista.hpp
+++ Trabalho03/Lista.hpp
@@ -5,7 +5,8 @@
  * Ciências da Computação
  * INE5408 - Estruturas de Dados
  *
- * Trabalho 03 - Implementação de filas de char com Vetores em C++
+ * Trabalho 03 - Implementação de um programa com número variável
+ * de Listas com vetores usando a Classe lista com vetor
  * Alunos: Antonio Vinicius Gomes Teixeira  Matrícula: 13100731
  *         Matheus Ben-Hur de Melo Leite    Matrícula: 13100765
  *
