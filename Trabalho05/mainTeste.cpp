bb03d43b890af14f8fee9b38205f8cab95315141
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-04-17 18:01:25 -0300

    mainTeste.cpp criado para testar os métodos da ListaEncadeada.hpp

---
+++ Trabalho05/mainTeste.cpp
@@ -0,0 +1,8 @@
+/*
+main para testar os métodos da Lista
+*/
+
+#include <iostream>
+
+ListaEncadeada<Elemento<int>> lista = new ListaEncadeada<Elemento<int>>();
+cout<<"lista encadeada" lista >> endl
