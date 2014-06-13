29a9bc89890350a9a0ae4f013b2533b7701ac790
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-06-13 11:44:58 -0300

    Trabalho criado, arvore AVL com inserção pronta, deleção feita porém sem balanceamento

---
+++ Trabalho07/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+
+#include "ArvoreAVL.h"
+
+int main (){
+    ArvoreAVL<char> arvore;
+    arvore.inserir('a');
+    arvore.inserir('c');
+    arvore.inserir('b');
+    cout << arvore.toString();
+}
