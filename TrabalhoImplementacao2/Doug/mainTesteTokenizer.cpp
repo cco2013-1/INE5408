9e20ea424e3a539bc1358c19f43a78edd19eeb5c
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2014-07-10 18:16:43 -0300

    filtter criado

--- TrabalhoImplementacao2/Doug/mainTesteTokenizer.cpp
+++ TrabalhoImplementacao2/Doug/mainTesteTokenizer.cpp
@@ -17,6 +17,7 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unordered_set> 
 #include "Lista.hpp"
 
 
@@ -24,24 +25,42 @@ using namespace std;
 
 
 Lista<string> lista;
-char text[] = "Matheus Ben-Hur/de Melo.Leite";
+Lista<string> lista2;
+char text[] = "Matheus Ben-Hur/de Melo.Leite or as but Luiz";
 char limiters[] = {' ', '-', '/','.'};
+unordered_set<string> conjunctions = {"and", "that", "but", "or", "as", "if", "when","than", "because", "while", "where", 
+"after", "so", "though", "since", "until", "whether", "before", "although", "nor", "like", "once", "unless", 
+"now", "except"};
 
 Lista<string> tokenizer(char text[], char limiters[]){
     Lista<string> words;
     char *word = strtok(text, limiters);
     for(int i = 0; word; i++) {
-        words.adiciona(string(word));
+        words.adiciona(string(word));    
         word = strtok(NULL, limiters);
     }
     return words;
 }
 
+Lista<string> filterConjunction(Lista<string> words, unordered_set<string> conjunctions) {
+    Lista<string> filtered;
+    int size = words.tamanho();
+    for(int i = 0; i < size; i++) {
+        string removed = words.retira();
+        unordered_set<string>::const_iterator got = conjunctions.find(removed);    
+        if(got == conjunctions.end()) {
+            filtered.adiciona(removed);
+        }
+    }
+    return filtered;
+}
+
 int main() {
     lista = tokenizer(text, limiters);
-    int tamanho = lista.tamanho();
+    lista2 = filterConjunction(lista, conjunctions);
+    int tamanho = lista2.tamanho();
     for(int i = 0; i < tamanho ; i++) {
-        cout << lista.retira() << endl;
+        cout << lista2.retira() << endl;
     }
     return 0;
 }
